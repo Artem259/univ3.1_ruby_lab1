5760de8f4a0a12081989dc313b09c5950cd086ab
Artem Poliakov <artem4250@gmail.com>
2022-09-03 20:27:53 +0300

    task.rb update

--- task.rb
+++ task.rb
@@ -7,9 +7,9 @@ x, b, y = x.to_f, b.to_f, y.to_f
 print "\nInput:\n x = #{x}\n b = #{b}\n y = #{y}\n\n"
 
 begin
-f = Math.acos(Math.sin(x/2) / Math.cbrt(b-1))
-f = f + Math.log(Math::E**y + 4.1 * 10**2.1) / Math.tan(Math::PI/4 + x/2)
-raise 'Expression cannot be evaluated' if f.infinite? || f.nan?
+  f = Math.acos(Math.sin(x/2) / Math.cbrt(b-1))
+  f = f + Math.log(Math::E**y + 4.1 * 10**2.1) / Math.tan(Math::PI/4 + x/2)
+  raise 'Expression cannot be evaluated' if f.infinite? || f.nan?
 rescue StandardError => error
   print 'Calculation error: ', error.message
 else
